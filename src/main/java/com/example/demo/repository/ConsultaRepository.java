b784bd3e032cbad36a5f15c8133cd109fcca7c6f
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-10-31 00:56:22 -0300

    Alterando banco de dados, deu certo

--- src/main/java/com/example/demo/repository/ConsultaRepository.java
+++ src/main/java/com/example/demo/repository/ConsultaRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Paciente extends JpaRepository<Paciente, Long> {
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 }
--- src/main/java/com/example/demo/repository/PacienteRepository.java
+++ src/main/java/com/example/demo/repository/PacienteRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Consulta extends JpaRepository<Consulta, Long> {
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
